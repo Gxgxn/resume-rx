25c4b39246ab9e9f5495b397e4610c2a720c0da2
Gxgxn <53408847+Gxgxn@users.noreply.github.com>
2025-09-23 11:09:40 -0300

    Home Page, Nav & Resume Section Setup

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient">ResumeRx</p>
+      </Link>
+      <Link to="/upload">
+        <p className="primary-button w-fit">Upload</p>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from 'constants/index';
+import type { Route } from './+types/home';
+import Navbar from '~/components/Navbar';
+import ResumeCard from '~/components/ResumeCard';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'ResumeRx' },
+    { name: 'description', content: 'Welcome to ResumeRx!' },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')]">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Applications. Manage Resumes. Get Hired.</h1>
+          <h2>ResumeRx &mdash; Your Career Tracker Made Simple</h2>
+        </div>
+      </section>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
