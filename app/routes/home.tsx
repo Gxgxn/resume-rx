83e594b8ec4a501224fa2f14d19bed58a82f72bc
Gxgxn <53408847+Gxgxn@users.noreply.github.com>
2025-09-23 12:01:21 -0300

    homepage complete, added puter boilerplate

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,18 +15,18 @@ export default function Home() {
     <main className="bg-[url('/images/bg-main.svg')]">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-8">
           <h1>Track Applications. Manage Resumes. Get Hired.</h1>
           <h2>ResumeRx &mdash; Your Career Tracker Made Simple</h2>
         </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
-      {resumes.length > 0 && (
-        <div className="resumes-section">
-          {resumes.map((resume) => (
-            <ResumeCard key={resume.id} resume={resume} />
-          ))}
-        </div>
-      )}
     </main>
   );
 }
